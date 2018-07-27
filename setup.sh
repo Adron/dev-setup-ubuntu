e979ab82cc2455e48d82f0cd67bab8fccbe71dda
Adron Hall <adronhall@gmail.com>
2018-07-27 16:08:34 -0700

    Order changes.

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ mkdir SETUPDIR
 cd SETUPDIR
 
 echo "Installing some basic utilities and related tooling."
-apt-get install curl tree jq git atop nmap build-essential exfat-fuse exfat-utils
+sudo apt-get install curl tree jq git atop nmap build-essential exfat-fuse exfat-utils
 
 echo "Installing .NET Core 2.1."
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
@@ -15,20 +15,6 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install dotnet-sdk-2.1
-
-echo "Installing Visual Studio"
-VISUALSTUDIOFILENAME="visualstudiocode.deb"
-wget -O $VISUALSTUDIOFILENAME https://go.microsoft.com/fwlink/?LinkID=760868
-sudo dpkg -i $VISUALSTUDIOFILENAME
-sudo apt-get install -f
-
-echo "Installing Jetbrains Toolbox."
-wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.0.1569.tar.gz
-tar -xzf jetbrains-toolbox-1.0.1569.tar.gz
-cd jetbrains-toolbox-1.0.1569/
-./jetbrains-toolbox
-cd ..
-rm -rf jetbrains*
 
 echo "Installing Go."
 VERSION="1.10.3"
@@ -44,6 +30,21 @@ wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh |
 nvm install 6.12.3
 nvm install node
 nvm alias default node
+
+echo "Installing Visual Studio"
+VISUALSTUDIOFILENAME="visualstudiocode.deb"
+wget -O $VISUALSTUDIOFILENAME https://go.microsoft.com/fwlink/?LinkID=760868
+sudo dpkg -i $VISUALSTUDIOFILENAME
+sudo apt-get install -f
+
+echo "Installing Jetbrains Toolbox."
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.0.1569.tar.gz
+tar -xzf jetbrains-toolbox-1.0.1569.tar.gz
+cd jetbrains-toolbox-1.0.1569/
+./jetbrains-toolbox
+cd ..
+rm -rf jetbrains*
+
 
 echo "Confirmation of Installations"
 
