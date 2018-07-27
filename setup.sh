b8b6ec00857def86eeef177f8ae4e2c1663c3432
Adron Hall <adronhall@gmail.com>
2018-07-27 13:36:35 -0700

    Adding Jetbrains Toolbox.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Hello, starting Ubuntu customization setup."
+
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.0.1569.tar.gz
+tar -xzf jetbrains-toolbox-1.0.1569.tar.gz
+cd jetbrains-toolbox-1.0.1569/
+./jetbrains-toolbox
+cd ..
+rm -rf jetbrains*
