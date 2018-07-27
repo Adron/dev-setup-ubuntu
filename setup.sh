15bf1ac1d48d5c0c49cc0af4debceb89f19fa6c4
Adron Hall <adronhall@gmail.com>
2018-07-27 13:49:01 -0700

    Adding Visual Studio Code.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Hello, starting Ubuntu customization setup."
+echo "Hello, starting Ubuntu setup."
+
+echo "Adding repositories and keys."
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get update
+sudo apt-get install code
+
 
 wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.0.1569.tar.gz
 tar -xzf jetbrains-toolbox-1.0.1569.tar.gz
@@ -8,3 +17,4 @@ cd jetbrains-toolbox-1.0.1569/
 ./jetbrains-toolbox
 cd ..
 rm -rf jetbrains*
+
