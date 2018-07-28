ab062a590b7df269a1e9bf93affff5aa9364b144
Adron Hall <adronhall@gmail.com>
2018-07-27 17:10:47 -0700

    Tweaks.

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ cd SETUPDIR
 sudo apt-get update
 
 echo "Installing fundamentally necessary dev tools."
-sudo apt-get install git
+sudo apt-get install git -y
 
 echo "Installing Go."
 VERSION="1.10.3"
@@ -37,8 +37,8 @@ wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-pr
 sudo dpkg -i packages-microsoft-prod.deb
 
 sudo apt-get update
-sudo apt-get install apt-transport-https
-sudo apt-get install dotnet-sdk-2.1
+sudo apt-get install apt-transport-https -y
+sudo apt-get install dotnet-sdk-2.1 -y
 
 source ~/.bashrc
 echo "Check it, Fini."
